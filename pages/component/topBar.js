e25646db973d953ac14e7776f2683377265bfccc
koozone <empakoo@empal.com>
2021-12-20 00:32:08 +0900

    [추가] tailwind 적용 시작

--- pages/component/topBar.js
+++ pages/component/topBar.js
@@ -6,23 +6,23 @@ export default function topBar() {
 	return (
 		<div className={styles.topBarContainer}>
 			<div className={styles.menuContainer}>
-			<ul className={styles.menu}>
-				<li className={styles.item}>
-					<Link href="/">
-						<a>main</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/view/chat">
-						<a>chat</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/view/room">
-						<a>room</a>
-					</Link>
-				</li>
-			</ul>
+				<ul className={styles.menu}>
+					<li className={styles.item}>
+						<Link href="/">
+							<a>main</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/view/chat">
+							<a>chat</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/view/room">
+							<a>room</a>
+						</Link>
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 import TopBar from './component/topBar';
 
 export default function Index() {
-	return <TopBar />;
+	return (
+		<>
+			<TopBar />
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<label>
+				<input type="checkbox" checked /> Browser default
+			</label>
+			<label>
+				<input type="checkbox" className="accent-pink-500" checked /> Customized
+			</label>
+		</>
+	);
 }
