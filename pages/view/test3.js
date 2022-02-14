2bfe62ce633f8c0babc1a40d7255b98cf0c2bd53
koozone <empakoo@empal.com>
2022-02-14 23:40:51 +0900

    [수정] variableArea 코드 작업

--- pages/view/test3.js
+++ pages/view/test3.js
@@ -17,10 +17,10 @@ export const MenuTitle = (props) => {
 export const VariableArea = (props) => {
 	const [sizeData, runSizeData] = UseData({
 		hasCapture: false,
-		circleLeft: 0,
+		circleLeft: 300,
 		circleTop: 0,
 		isDragging: false,
-		previousLeft: 9999,
+		previousLeft: 0,
 		previousTop: 0,
 	});
 
@@ -31,7 +31,7 @@ export const VariableArea = (props) => {
 		// We store the initial coordinates to be able to calculate the changes
 		// later on.
 		extractPositionDelta(event);
-		console.log('onDown', sizeData);
+		// console.log('onDown', sizeData);
 	};
 
 	const onMove = (event) => {
@@ -46,7 +46,7 @@ export const VariableArea = (props) => {
 
 	const onUp = (event) => {
 		runSizeData.change('isDragging', false);
-		console.log('onUp', sizeData);
+		console.log('onUp', event);
 	};
 	const onGotCapture = (event) => runSizeData.change('hasCapture', true);
 	const onLostCapture = (event) => runSizeData.change('hasCapture', false);
@@ -58,34 +58,39 @@ export const VariableArea = (props) => {
 			left: left - sizeData.previousLeft,
 			top: top - sizeData.previousTop,
 		};
+
 		runSizeData.change('previousLeft', left);
 		runSizeData.change('previousTop', top);
+
 		return delta;
 	};
 
 	return (
 		<div className={`relative bg-gray-500 rounded-lg ring-1 ring-gray-900 ring-opacity-5 overflow-hidden`}>
-			<div className="block">
-				<div className="min-w-full sm:min-w-[400px] relative sm:pr-4" style={{maxWidth: '100%', width: `${sizeData.previousLeft}px`}}>
-					<div className="bg-white h-auto">
-						<div className="h-80"></div>
-						<div
-							className={`sr-only sm:not-sr-only sm:border-l sm:${sizeData.hasCapture ? 'bg-red-500' : 'bg-gray-100'} sm:absolute sm:right-0 sm:inset-y-0 sm:flex sm:justify-center sm:items-center sm:w-40 cursor-[ew-resize]`}
-							onPointerDown={onDown}
-							onPointerMove={onMove}
-							onPointerUp={onUp}
-							onPointerCancel={onUp}
-							onGotPointerCapture={onGotCapture}
-							onLostPointerCapture={onLostCapture}
-						>
-							<div className="absolute inset-y-0 -inset-x-2"></div>
-							<svg aria-hidden="true" className="h-4 w-4 text-gray-600 pointer-events-none" fill="currentColor" viewBox="0 0 24 24">
-								<path d="M8 5h2v14H8zM14 5h2v14h-2z"></path>
-							</svg>
-						</div>
+			{/* <div className="block"> */}
+			{/* <div className="min-w-full sm:min-w-[0px] relative sm:pr-4" style={{maxWidth: '100%', width: `${sizeData.previousLeft}px`}}> */}
+			<div className="min-w-full max-w-full sm:min-w-[300px] relative sm:pr-[100px]" style={{width: `${sizeData.circleLeft + 100}px`}}>
+				<div className="bg-white h-auto">
+					<div className="h-80"></div>
+					<div
+						// className={`sr-only sm:not-sr-only sm:border-l sm:${sizeData.hasCapture ? 'bg-red-500' : 'bg-gray-100'} sm:absolute sm:right-0 sm:inset-y-0 sm:flex sm:justify-center sm:items-center sm:w-[100px] cursor-[ew-resize]`}
+						className={`sr-only sm:not-sr-only sm:border-l ${sizeData.hasCapture ? 'sm:bg-red-200' : 'sm:bg-gray-100'} sm:absolute sm:right-0 sm:inset-y-0 sm:flex sm:justify-center sm:items-center sm:w-[100px] cursor-[ew-resize]`}
+						onPointerDown={onDown}
+						onPointerMove={onMove}
+						onPointerUp={onUp}
+						onPointerCancel={onUp}
+						onGotPointerCapture={onGotCapture}
+						onLostPointerCapture={onLostCapture}
+						// style={{left: `${sizeData.circleLeft}px`, top: `${sizeData.circleTop}px`}}
+					>
+						<div className="absolute inset-y-0 -inset-x-2"></div>
+						<svg aria-hidden="true" className="h-4 w-4 text-gray-600 pointer-events-none" fill="currentColor" viewBox="0 0 24 24">
+							<path d="M8 5h2v14H8zM14 5h2v14h-2z"></path>
+						</svg>
 					</div>
 				</div>
 			</div>
+			{/* </div> */}
 		</div>
 	);
 };
