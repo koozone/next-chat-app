905ee503043ae7f791c208136ba6598b7de8d44f
koozone <empakoo@empal.com>
2022-02-23 15:34:40 +0900

    [수정] calendar 코드 작업

--- pages/avengers/module/calendarModule.js
+++ pages/avengers/module/calendarModule.js
@@ -1,5 +1,6 @@
 import {DateTime} from 'luxon';
 import {Button, Toggle} from '../../component/ui_ds7';
+import {SelectBox} from '../../view/sampleSelectbox2';
 import ToyCalendar from '../../view/toyCalendar';
 import PhotoItem, {PhotoRoundItem} from '../item/photoItem';
 import PhotoList from '../list/photoList';
@@ -7,65 +8,137 @@ import PhotoSection from '../section/photoSection';
 import {UseData} from '../vision';
 
 export const CalendarModule = () => {
-	const data = UseData({
+	const calendarData = UseData({
 		dateTime: DateTime.local(2022, 9, 3),
+		viewWeekNumber: true,
+		viewWeekString: true,
+		weekStringShot: true,
+		viewSubDate: true,
+		weekLocale: [{value: 'ko', label: '한글'}],
+		startWeekDay: [{value: 7, label: '일'}],
 	});
-	const [stateData, runStateData] = data;
+	const [calendarState, runCalendarState] = calendarData;
 
 	return (
 		<div className="p-10">
-			<span className="text-3xl">{`${stateData.dateTime.year} ${stateData.dateTime.setLocale('ko')[false ? 'monthShort' : 'monthLong']}`}</span>
-			<ToyCalendar data={data} />
+			<div className="text-3xl">{`${calendarState.dateTime.year} ${calendarState.dateTime.setLocale('ko')[false ? 'monthShort' : 'monthLong']}`}</div>
+			<div className="inline-block w-1/2">
+				<ToyCalendar height="h-[400px]" data={calendarData} />
+			</div>
+			{/* <div className="inline-block w-1/2">
+				<ToyCalendar height="h-[400px]" data={calendarData} />
+			</div> */}
 
-			<Button
-				theme="default-IL-md-md-md"
-				icon="bxs-chevrons-left"
-				// text=""
-				onClick={() => {
-					runStateData.change('dateTime', stateData.dateTime.plus({years: -1}));
-				}}
-			/>
-			<Button
-				theme="default-IL-md-md-md"
-				icon="bxs-chevron-left"
-				text="Prev"
-				onClick={() => {
-					runStateData.change('dateTime', stateData.dateTime.plus({months: -1}));
-				}}
-			/>
-			<Button
-				theme="default-IL-md-md-md"
-				icon="bx-leaf"
-				text="Today"
-				onClick={() => {
-					runStateData.change('dateTime', DateTime.now());
-				}}
-			/>
-			<Button
-				theme="default-IL-md-md-md"
-				icon="bx-leaf"
-				text="Init"
-				onClick={() => {
-					runStateData.reset('dateTime');
-				}}
-			/>
-			<Button
-				theme="default-IL-md-md-md"
-				iconR="bxs-chevron-right"
-				text="Next"
-				onClick={() => {
-					runStateData.change('dateTime', stateData.dateTime.plus({months: 1}));
-				}}
-			/>
-			<Button
-				theme="default-IL-md-md-md"
-				icon="bxs-chevrons-right"
-				// text=""
-				onClick={() => {
-					runStateData.change('dateTime', stateData.dateTime.plus({years: 1}));
-				}}
-			/>
-			<span>{stateData.dateTime.toString()}</span>
+			<div className="p-5 space-x-3 flex justify-center items-center flex-wrap ring-2 ring-gray-500 rounded-lg">
+				<Button
+					theme="default-IL-md-md-md"
+					icon="bxs-chevrons-left"
+					// text=""
+					onClick={() => {
+						runCalendarState.change('dateTime', calendarState.dateTime.plus({years: -1}));
+					}}
+				/>
+				<Button
+					theme="default-IL-md-md-md"
+					icon="bxs-chevron-left"
+					text="Prev"
+					onClick={() => {
+						runCalendarState.change('dateTime', calendarState.dateTime.plus({months: -1}));
+					}}
+				/>
+				<Button
+					theme="default-IL-md-md-md"
+					icon="bx-leaf"
+					text="Today"
+					onClick={() => {
+						runCalendarState.change('dateTime', DateTime.now());
+					}}
+				/>
+				<Button
+					theme="default-IL-md-md-md"
+					iconR="bxs-chevron-right"
+					text="Next"
+					onClick={() => {
+						runCalendarState.change('dateTime', calendarState.dateTime.plus({months: 1}));
+					}}
+				/>
+				<Button
+					theme="default-IL-md-md-md"
+					icon="bxs-chevrons-right"
+					// text=""
+					onClick={() => {
+						runCalendarState.change('dateTime', calendarState.dateTime.plus({years: 1}));
+					}}
+				/>
+			</div>
+			<div className="p-5 space-x-3 flex justify-center items-center flex-wrap ring-2 ring-gray-500 rounded-lg">
+				<Toggle
+					theme="default-IL-md-md-md::danger-IL-md-md-md"
+					icon="bxs-chevrons-right::bx-leaf"
+					checked={calendarState.viewWeekNumber}
+					onChange={() => {
+						runCalendarState.change('viewWeekNumber', !calendarState.viewWeekNumber);
+					}}
+				/>
+				<Toggle
+					theme="default-IL-md-md-md::danger-IL-md-md-md"
+					icon="bxs-chevrons-right::bx-leaf"
+					checked={calendarState.viewWeekString}
+					onChange={() => {
+						runCalendarState.change('viewWeekString', !calendarState.viewWeekString);
+					}}
+				/>
+				<Toggle
+					theme="default-IL-md-md-md::danger-IL-md-md-md"
+					icon="bxs-chevrons-right::bx-leaf"
+					checked={calendarState.viewSubDate}
+					onChange={() => {
+						runCalendarState.change('viewSubDate', !calendarState.viewSubDate);
+					}}
+				/>
+				<SelectBox
+					value="startWeekDay"
+					direction={false}
+					data={calendarData}
+					options={[
+						{value: 7, label: '일'},
+						{value: 1, label: '월'},
+						{value: 2, label: '화'},
+						{value: 3, label: '수'},
+						{value: 4, label: '목'},
+						{value: 5, label: '금'},
+						{value: 6, label: '토'},
+					]}
+					width="w-[100px]"
+				/>
+				<SelectBox
+					value="weekLocale"
+					direction={false}
+					data={calendarData}
+					options={[
+						{value: 'ko', label: '한글'},
+						{value: 'en', label: '영어'},
+						{value: 'ja', label: '한자'},
+					]}
+					width="w-[100px]"
+				/>
+				<Toggle
+					theme="default-IL-md-md-md::danger-IL-md-md-md"
+					icon="bxs-chevrons-right::bx-leaf"
+					checked={calendarState.weekStringShot}
+					onChange={() => {
+						runCalendarState.change('weekStringShot', !calendarState.weekStringShot);
+					}}
+				/>
+				<Button
+					theme="default-IL-md-md-md"
+					icon="bx-leaf"
+					text="Reset"
+					onClick={() => {
+						runCalendarState.reset();
+					}}
+				/>
+			</div>
 		</div>
 	);
 };
--- pages/view/toyCalendar.js
+++ pages/view/toyCalendar.js
@@ -16,84 +16,74 @@ export const ToyCalendar = (props) => {
 	const {width = 'w-full', height = 'h-[500px]', data} = props;
 	const [stateData, runStateData] = data || UseData({dateTime: DateTime.now()});
 
-	const startWeekMonday = false;
-	const viewWeekNumber = true;
-	const viewWeekString = true;
-	const weekStringShot = true;
-	const weekLocale = 'en';
-	const viewSubDate = true;
+	// const startWeekDay = 7; // 월요일(1), 화요일(2)...일요일(7)
+	const startWeekDay = stateData.startWeekDay?.map((val) => val.value)[0]; // 월요일(1), 화요일(2)...일요일(7)
+	const viewWeekNumber = stateData.viewWeekNumber;
+	const viewWeekString = stateData.viewWeekString;
+	const weekStringShot = stateData.weekStringShot;
+	// const weekLocale = 'ja';
+	const weekLocale = stateData.weekLocale?.map((val) => val.value).join(', ');
+	const viewSubDate = stateData.viewSubDate;
 	const subDateLocale = 'chinese';
-	const colList = ['0.25fr', '1fr', '1fr', '1fr', '1fr', '1fr', '0.5fr', '0.5fr'];
-	const rowList = ['0.5fr', '1fr', '1fr', '1fr', '1fr', '1fr'];
+	const colList = ['30px', '1fr', '1fr', '1fr', '1fr', '1fr', '0.5fr', '0.5fr'];
+	const rowList = ['40px', '1fr', '1fr', '1fr', '1fr', '1fr', '1fr'];
 
-	// const dateTime = DateTime.fromISO('2022-W02-1');
-	// const nowDateTime = DateTime.local(2022, 9, 3);
 	const nowDateTime = stateData?.dateTime || DateTime.now();
-	const startFirstDateTime = nowDateTime.startOf('month');
-	const startMondayDateTime = startFirstDateTime.minus({days: (startFirstDateTime.weekday - 1) % 7});
-	const startViewDateTime = startMondayDateTime.minus({days: startWeekMonday ? 0 : 1});
-	// const weekday = startViewDateTime.weekday;
-	// const weekNumber = dateTime.startOf('month').weekNumber;
-	// const startViewDateTime = DateTime.fromISO(`${dateTime.year}-W${String(weekNumber).padStart(2, '0')}-1`);
-	// const dateTime3 = nowDateTime.endOf('month');
-	// const dateTime2 = nowDateTime.plus({hours: 3, minutes: 2});
-	// var f = {month: 'long', day: 'numeric'};
+	const startDateTime = nowDateTime.startOf('month');
+	const endDateTime = nowDateTime.endOf('month');
+	const viewDateTime = startDateTime.minus({days: (7 + startDateTime.weekday - startWeekDay) % 7});
+	const weekLenght = Math.ceil(endDateTime.diff(viewDateTime, 'days').as('days') / 7);
 
-	// const gridCols = classNames(
-	// 	startWeekMonday
-	// 		? {
-	// 				[`grid-cols-[40px_80px_80px_80px_80px_80px_40px_40px]`]: viewWeekNumber,
-	// 				'grid-cols-[80px_80px_80px_80px_80px_40px_40px]': !viewWeekNumber,
-	// 		  }
-	// 		: {
-	// 				'grid-cols-[40px_40px_80px_80px_80px_80px_80px_40px]': viewWeekNumber,
-	// 				'grid-cols-[40px_80px_80px_80px_80px_80px_40px]': !viewWeekNumber,
-	// 		  }
-	// );
-	const aaa = useCallback((valueList, indexList) => {
-		return indexList.map((val) => valueList[val]).join(' ');
-	}, []);
+	const getGridRows = useCallback(() => {
+		const weekRowList = viewWeekString ? [rowList[0]] : [];
+		const dayRowList = [...Array(weekLenght)].map((val, index) => {
+			const addNumber = 1;
+			const rowIndex = index + addNumber;
 
-	const gridRows = aaa(rowList, [0, 1, 2, 3, 4, 5]);
-	const gridCols = classNames(
-		startWeekMonday
-			? {
-					[`${aaa(colList, [0, 1, 2, 3, 4, 5, 6, 7])}`]: viewWeekNumber,
-					[`${aaa(colList, [1, 2, 3, 4, 5, 6, 7])}`]: !viewWeekNumber,
-			  }
-			: {
-					[`${aaa(colList, [0, 7, 1, 2, 3, 4, 5, 6])}`]: viewWeekNumber,
-					[`${aaa(colList, [7, 1, 2, 3, 4, 5, 6])}`]: !viewWeekNumber,
-			  }
-	);
-	console.log('weekYear', DateTime.local(2022, 1, 3).weekYear, DateTime.local(2022, 1, 3).weekNumber, startMondayDateTime.toString());
+			return rowList[rowIndex];
+		});
+
+		return [...weekRowList, ...dayRowList].join(' ');
+	}, [weekLenght, viewWeekString]);
+
+	const getGridCols = useCallback(() => {
+		const weekColList = viewWeekNumber ? [colList[0]] : [];
+		const dayColList = [...Array(7)].map((val, index) => {
+			const addNumber = 1;
+			const colIndex = ((index + (startWeekDay - 1)) % 7) + addNumber;
+
+			return colList[colIndex];
+		});
+
+		return [...weekColList, ...dayColList].join(' ');
+	}, [startWeekDay, viewWeekNumber]);
+
 	return (
-		<div className={classNames(width, height, 'grid gap-px rounded-lg ring-2 ring-slate-500 bg-slate-300 overflow-hidden')} style={{gridTemplateColumns: gridCols, gridTemplateRows: gridRows}}>
+		<div className={classNames(width, height, 'grid gap-px rounded-lg ring-2 ring-slate-500 bg-slate-300 overflow-hidden')} style={{gridTemplateColumns: getGridCols(), gridTemplateRows: getGridRows()}}>
 			{viewWeekString && (
 				<Fragment>
 					{viewWeekNumber && <div className="bg-white"></div>}
 					{[...Array(7)].map((dayVal, k) => {
 						const index = k;
-						const weekIndex = (index + (startWeekMonday ? 1 : 0)) % 7;
-						const viewDateTime = startViewDateTime.plus({days: index});
-						const bgColor = classNames('bg-slate-100');
-						const textColor = classNames({
+						const weekIndex = (index + startWeekDay) % 7;
+						const mainDateTime = viewDateTime.plus({days: index});
+						const mainBgColor = classNames('bg-slate-100');
+						const mainTextColor = classNames({
 							'text-slate-500': weekIndex != 0 && weekIndex != 6,
 							'text-blue-500': weekIndex == 6,
 							'text-red-500': weekIndex == 0,
 						});
 
 						return (
-							<DayItem key={`weekString${index}`} className={classNames(bgColor, 'justify-center items-center')}>
-								<div className={classNames(textColor, 'text-md')}>{viewDateTime.setLocale(weekLocale)[weekStringShot ? 'weekdayShort' : 'weekdayLong']}</div>
+							<DayItem key={`weekString${index}`} className={classNames(mainBgColor, 'justify-center items-center')}>
+								<div className={classNames(mainTextColor, 'text-md')}>{mainDateTime.setLocale(weekLocale)[weekStringShot ? 'weekdayShort' : 'weekdayLong']}</div>
 							</DayItem>
 						);
 					})}
 				</Fragment>
 			)}
-			{[...Array(5)].map((weekVal, i) => {
-				// const weekDateTime = startViewDateTime.plus({weeks: i});
-				const weekDateTime = startMondayDateTime.plus({weeks: i});
+			{[...Array(weekLenght)].map((weekVal, i) => {
+				const weekDateTime = viewDateTime.plus({weeks: i});
 
 				return (
 					<Fragment key={`weekNumber${i}`}>
@@ -104,32 +94,32 @@ export const ToyCalendar = (props) => {
 						)}
 						{[...Array(7)].map((dayVal, k) => {
 							const index = k + i * 7;
-							const weekIndex = (index + (startWeekMonday ? 1 : 0)) % 7;
-							const viewDateTime = startViewDateTime.plus({days: index});
-							const subDateTime = viewDateTime.reconfigure({outputCalendar: subDateLocale});
-							const bgColor = classNames({
+							const weekIndex = (index + startWeekDay) % 7;
+							const mainDateTime = viewDateTime.plus({days: index});
+							const subDateTime = mainDateTime.reconfigure({outputCalendar: subDateLocale});
+							const mainBgColor = classNames({
 								'bg-white': weekIndex != 0 && weekIndex != 6,
 								'bg-blue-50': weekIndex == 6,
 								'bg-red-50': weekIndex == 0,
 							});
-							const textColor = classNames({
+							const mainTextColor = classNames({
 								'text-slate-500': weekIndex != 0 && weekIndex != 6,
 								'text-blue-500': weekIndex == 6,
 								'text-red-500': weekIndex == 0,
 							});
-							const opacity = classNames({
-								'opacity-30': viewDateTime.month != nowDateTime.month,
-								'opacity-100': viewDateTime.month == nowDateTime.month,
+							const mainOpacity = classNames({
+								'opacity-30': mainDateTime.month != nowDateTime.month,
+								'opacity-100': mainDateTime.month == nowDateTime.month,
 							});
-							const bbb = classNames({
+							const subVisible = classNames({
 								invisible: !['1', '5', '10', '15', '20', '25', '30'].includes(subDateTime.toFormat('d')),
 								visible: ['1', '5', '10', '15', '20', '25', '30'].includes(subDateTime.toFormat('d')),
 							});
 
 							return (
-								<DayItem key={`dayNumber${index}`} className={classNames(bgColor, opacity, 'justify-center items-center')}>
-									<div className={classNames(textColor, 'text-3xl')}>{viewDateTime.day}</div>
-									{viewSubDate && <div className={classNames(bbb, 'text-xs text-slate-400')}>{subDateTime.toFormat('L.d')}</div>}
+								<DayItem key={`dayNumber${index}`} className={classNames(mainBgColor, mainOpacity, 'justify-center items-center')}>
+									<div className={classNames(mainTextColor, 'text-3xl')}>{mainDateTime.day}</div>
+									{viewSubDate && <div className={classNames(subVisible, 'text-xs text-slate-400')}>{subDateTime.toFormat('L.d')}</div>}
 								</DayItem>
 							);
 						})}
